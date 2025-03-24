f0603f08467381ca5f3782f3a08c3f0eb924900b
majodr13 <mduranramirez13@gmail.com>
2025-03-24 07:04:16 -0600

    Falta que la foto se jale al main y botón de logout

--- integradora/integradora/settings.py
+++ integradora/integradora/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = 'django-insecure-kz1cy5gbag*aum8a4!km&*y1i3-v6+43v3l(kju1a=m)_tv5%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '192.168.1.67', '10.1.124.135', '192.168.1.254', '10.1.125.24', '192.168.248.228', '10.1.124.172', '192.168.1.69']
-
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '192.168.1.67', '10.1.124.135', '192.168.1.254', '10.1.125.24', '192.168.248.228', '10.1.124.172', '192.168.1.69', '10.1.124.115']
 
 
 # Application definition
